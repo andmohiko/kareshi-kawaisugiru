9ea99a41720cfec0a391f7e646fdeac51d49473f
andmohiko <andmohiko@gmail.com>
2024-11-17 02:46:59 +0900

    feat: add next seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { NextSeo } from 'next-seo'
 import { TopContainer } from '~/features/Top/components/TopContainer'
 
 const IndexPage = () => {
-  return <TopContainer />
+  return (
+    <>
+      <NextSeo
+        title="j"
+        description="彼氏がかわいすぎる.com"
+        nofollow={false}
+        canonical={'http://xn--n8jnck8c3rya5127g0wxa.com/'}
+        openGraph={{
+          title: '彼氏がかわいすぎる.com',
+          description: '彼氏がかわいすぎる.com',
+          url: 'http://xn--n8jnck8c3rya5127g0wxa.com/',
+          type: 'website',
+          images: [
+            {
+              url: '/images/ogp.png',
+              width: 800,
+              height: 600,
+              alt: '',
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
+      <TopContainer />
+    </>
+  )
 }
 
 export default IndexPage
