0942d8fc74dc5e38c6bf809a374719d8e4e27449
andmohiko <andmohiko@gmail.com>
2024-11-17 15:31:09 +0900

    feat: add base layout to top page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 
 import '~/styles/reset.css'
 import '~/styles/globals.css'
+import '~/styles/variables.css'
 import { Providers } from '~/providers'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
