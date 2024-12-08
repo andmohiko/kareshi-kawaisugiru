e51e8c5770815605da09d551b0f546757c7637aa
andmohiko <andmohiko@gmail.com>
2024-12-08 22:18:24 +0900

    feat: add top logo

--- src/features/top/components/TopContainer/index.tsx
+++ src/features/top/components/TopContainer/index.tsx
@@ -1,12 +1,18 @@
 import { BasicButton } from '~/components/Buttons/BasicButton'
 import styles from './style.module.css'
 import { BaseLayout } from '~/components/Layouts/BaseLayout'
+import Image from 'next/image'
 
 export const TopContainer = (): React.ReactNode => {
   return (
     <BaseLayout>
       <div className={styles.container}>
-        <h1 className={styles.title}>彼氏がかわいすぎる</h1>
+        <Image
+          src="/images/logo-black.webp"
+          width={329}
+          height={191}
+          alt="彼氏がかわいすぎる.com"
+        />
         <div className={styles.action}>
           <BasicButton href="/i/mypage">彼氏のページを作る</BasicButton>
         </div>
