a26707d95824d9de9f38ad8bbe00148c4748d955
andmohiko <andmohiko@gmail.com>
2024-12-08 19:29:24 +0900

    fix: primary color

--- src/features/top/components/TopContainer/index.tsx
+++ src/features/top/components/TopContainer/index.tsx
@@ -7,10 +7,9 @@ export const TopContainer = (): React.ReactNode => {
     <BaseLayout>
       <div className={styles.container}>
         <h1 className={styles.title}>彼氏がかわいすぎる</h1>
-        {/* TODO: 機能を公開する */}
-        {/* <div className={styles.action}>
+        <div className={styles.action}>
           <BasicButton href="/i/mypage">彼氏のページを作る</BasicButton>
-        </div> */}
+        </div>
       </div>
     </BaseLayout>
   )
