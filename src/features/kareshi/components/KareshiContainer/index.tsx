fa871c0df0864cecc36051ba70263551ac23e486
andmohiko <andmohiko@gmail.com>
2024-12-08 18:46:28 +0900

    feat: use portrait image in sp

--- src/features/kareshi/components/KareshiContainer/index.tsx
+++ src/features/kareshi/components/KareshiContainer/index.tsx
@@ -1,25 +1,47 @@
+import { useMediaQuery } from '@mantine/hooks'
 import styles from './style.module.css'
-import { BaseLayout } from '~/components/Layouts/BaseLayout'
-import { TitleText } from '~/components/Typography/TitleText'
 
 import { Kareshi } from '~/entities/Kareshi'
+import { FullScreenLayout } from '~/components/Layouts/FullScreenLayout'
 
 type Props = {
   kareshi: Kareshi
 }
 
 export const KareshiContainer = ({ kareshi }: Props): React.ReactNode => {
+  const isUsePortrait = useMediaQuery('(max-width: 600px)')
+  const isUseSquare = useMediaQuery('(max-width: 950px)')
+
+  const kareshiImageUrl = () => {
+    // 画面が縦のときはなるべく縦の画像を使う
+    if (isUsePortrait) {
+      if (kareshi.portraitImageUrl) {
+        return kareshi.portraitImageUrl
+      }
+      if (kareshi.squareImageUrl) {
+        return kareshi.squareImageUrl
+      }
+      return kareshi.portraitImageUrl ?? ''
+    }
+    // 画面が正方形のときはなるべく正方形の画像を使う
+    if (isUseSquare) {
+      if (kareshi.squareImageUrl) {
+        return kareshi.squareImageUrl
+      }
+      return kareshi.landscapeImageUrl ?? ''
+    }
+    return kareshi.landscapeImageUrl ?? ''
+  }
   return (
-    <BaseLayout>
-      <div className={styles.container}>
-        <>
-          <TitleText>{kareshi.kareshiName}</TitleText>
-          <img
-            src={kareshi.landscapeImageUrl ?? ''}
-            alt={kareshi.kareshiName ?? ''}
-          />
-        </>
+    <FullScreenLayout>
+      <div
+        className={styles.container}
+        style={{
+          backgroundImage: `url(${kareshiImageUrl()})`,
+        }}
+      >
+        <h1 className={styles.name}>{kareshi.kareshiName}</h1>
       </div>
-    </BaseLayout>
+    </FullScreenLayout>
   )
 }
